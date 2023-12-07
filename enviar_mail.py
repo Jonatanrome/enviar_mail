f422a020486696b0d810ee10ff32d28b09acb800
Jonatanrome <89875223+Jonatanrome@users.noreply.github.com>
2023-12-07 11:34:06 -0300

    Update enviar_mail.py

--- enviar_mail.py
+++ enviar_mail.py
@@ -1,1 +1,13 @@
 
+import os
+import smtplib
+from email.message import EmailMessage
+
+user_gmail = os.getenv('')
+password_gmail = ""
+
+def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
+    # aqui va tu codigo
+    ...
+
+send_notifying_mail(user_gmail, password_gmail)
